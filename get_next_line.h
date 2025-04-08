24304f9ca4c1618bc3aaba4f5e2b43d2fdfe4726
Aram <gourj666@gmail.com>
2025-04-09 01:34:03 +0400

    Added the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agurdzhi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 21:04:26 by agurdzhi          #+#    #+#             */
-/*   Updated: 2025/04/06 21:09:08 by agurdzhi         ###   ########.fr       */
+/*   Updated: 2025/04/09 01:33:20 by agurdzhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,12 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+char	*ft_read_fd(int fd, char *read_into);
+char	*ft_append(char *s1, char *s2);
+char	*ft_extract_line(char *str);
+char	*ft_line_trim(char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int ch);
+size_t	ft_strlen(const char *str);
 
 #endif
